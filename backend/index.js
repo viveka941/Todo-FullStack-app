f28584717b9e835df011a9b73b80eec25c9ca388
Vivek kumar <viveka9415@gmail.com>
2025-05-23 11:46:59 +0530

    add router for user register route

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
-import exprss from 'express'
+import express from 'express'
 import connectDb from './utils/connectDB.js'
+import userRoute from './router/user.router.js'
 
-const app = exprss()
+const app = express()
+app.use(express.json())
 
+app.use("/api",userRoute)
 app.listen(5000,()=>{
   connectDb()
   console.log("Server is running port number 5000")
